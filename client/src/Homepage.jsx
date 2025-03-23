b530ac3bd565c64db1133336f29ae3004a3a6a14
aniketc22 <aniketc22@gmail.com>
2025-03-23 17:54:20 +0530

    i

---
+++ client/src/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Heading, Text } from "@chakra-ui/react";
+import { InputForm } from "./components/InputForm";
+export default function Homepage() {
+  return (
+    <>
+          <Heading >
+        URL Shortener
+          </Heading>
+          <InputForm />
+         
+    </>
+  )
+}
